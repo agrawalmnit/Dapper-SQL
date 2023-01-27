bd9fb331ba8629a730cd2d50e3c6d4eab474a7ab
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2023-01-27 09:59:46 +0530

    Delete WebApplication9 directory

--- WebApplication9/UserDB/dbo/StoredProcedures/Insert.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[Insert]
-	@FirstName NVARCHAR(255),
-	@LastName NVARCHAR(255)
-AS
-BEGIN
-	INSERT INTO [dbo].[Insert](FirstName, LastName)
-	VALUES(@FirstName,@LastName);
-END
